6c26ae190d3e14ea999485a940056cbbf22cf786
AurelijaZuba <auryte00@hotmail.com>
2019-07-01 15:42:11 +0100

    Winning status implementation

--- src/main/java/com/codurance/TicTacToeGame.java
+++ src/main/java/com/codurance/TicTacToeGame.java
@@ -1,5 +1,8 @@
 package com.codurance;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TicTacToeGame {
     private final BoardState boardState;
     private Player currentPlayer;
@@ -35,9 +38,20 @@ public class TicTacToeGame {
     public GameStatus status() {
         if(boardState.emptyBoard())
             return GameStatus.START_OF_GAME;
+        if(gameHasBeenWon())
+            return GameStatus.WON;
         if(!boardState.emptyBoard() && boardState.numberOfMoves() != 9)
             return GameStatus.IN_PLAY;
 
         return GameStatus.DRAW;
+    }
+
+    private boolean gameHasBeenWon() {
+        BoardState winningBoard = new BoardState();
+        winningBoard.placeMarker(new PlayerMove(new Position(0,0), Player.X));
+        winningBoard.placeMarker(new PlayerMove(new Position(0,1), Player.X));
+        winningBoard.placeMarker(new PlayerMove(new Position(0,2), Player.X));
+
+        return winningBoard.equals(boardState);
     }
 }
