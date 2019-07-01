882ae279007358b9639b2c97a50eacfa11bdea1c
AurelijaZuba <auryte00@hotmail.com>
2019-07-01 15:51:38 +0100

    failing state for winning condition

--- src/main/java/com/codurance/TicTacToeGame.java
+++ src/main/java/com/codurance/TicTacToeGame.java
@@ -47,11 +47,8 @@ public class TicTacToeGame {
     }
 
     private boolean gameHasBeenWon() {
-        BoardState winningBoard = new BoardState();
-        winningBoard.placeMarker(new PlayerMove(new Position(0,0), Player.X));
-        winningBoard.placeMarker(new PlayerMove(new Position(0,1), Player.X));
-        winningBoard.placeMarker(new PlayerMove(new Position(0,2), Player.X));
 
-        return winningBoard.equals(boardState);
+
+        return false;
     }
 }
