4453b5c61a882d4c3ca5edc9551029b59aaa7619
AurelijaZuba <auryte00@hotmail.com>
2019-07-01 10:41:43 +0100

    failing test for x player marking first position

---
+++ src/main/java/com/codurance/TicTacToeGame.java
@@ -0,0 +1,9 @@
+package com.codurance;
+
+import java.util.HashMap;
+
+public class TicTacToeGame {
+    public HashMap position(String player, String position) {
+            return null;
+    }
+}
--- src/test/java/com/codurance/TicTacToeGameShould.java
+++ src/test/java/com/codurance/TicTacToeGameShould.java
@@ -1,4 +1,21 @@
 package com.codurance;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TicTacToeGameShould {
+
+    @Test
+    void allow_first_player_X_to_mark_any_position() {
+        TicTacToeGame game = new TicTacToeGame();
+        HashMap gameState = game.position("X", "0,2");
+
+        HashMap expected = new HashMap();
+        expected.put("0,2", "X");
+
+        assertThat(gameState).isEqualTo(expected);
+    }
 }
